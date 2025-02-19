1691a34a9891ad3e420e2327cef4c56e855ae828
Adebayo Temitope <tsadebayo@myseneca.ca>
2025-02-18 23:13:18 -0500

    added build script

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Run CMake and make
+echo "Configuring project with CMake..."
+cmake . || { echo "CMake configuration failed!"; exit 1; }
+
+echo "Creating executable file..."
+make || { echo "Build failed!"; exit 1; }
